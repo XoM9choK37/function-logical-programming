3bb810eee92567f4e26ff951c070e933b8e0fd9f
XoM9choK37 <sanya.sasha.kostrov@mail.ru>
2026-03-08 02:17:28 +0300

    Add files

---
+++ theory/Облако/F#/ConsoleApplication22/ConsoleApplication22/Program.fs
@@ -0,0 +1,10 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+let rec cifrSum n = 
+    if n = 0 then 0
+    else (n%10) + (cifrSum (n / 10))
+[<EntryPoint>]
+let main argv = 
+    System.Convert.ToInt32(System.Console.ReadLine()) |> cifrSum |> System.Console.WriteLine
+    let z = System.Console.ReadKey()
+    0 // возвращение целочисленного кода выхода
---
+++ theory/Облако/F#/ConsoleApplication24/ConsoleApplication24/Program.fs
@@ -0,0 +1,62 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+// Дополнительную справку см. в проекте "Учебник по F#".
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+let f2 list = List.nth list (List.findIndex (fun x -> x = (List.max (List.map (fun el -> List.length (List.filter (fun elem -> (elem = el)) list)) list))) (List.map (fun el -> List.length (List.filter (fun elem -> (elem = el)) list)) list))   
+
+let f3 list = Set.toList (Set.ofList (List.filter (fun x -> (((x % 2) = 0)&&(((List.length (List.filter (fun elem -> elem = x) list)) % 2) = 0))) list))
+
+
+let rec cifrSum (n : int) : int = 
+    if n = 0 then 0
+    else (n%10) + (cifrSum (n / 10))
+
+let f4 (list : 'int list) = List.filter (fun x -> (((x % 2) = 0) || (((cifrSum x) % 2) = 0)))
+
+let f5 list = List.length (List.filter (fun x -> (List.exists (fun el -> el * el = x) list)) list)
+
+let delCount n = 
+    let rec delCount n del count = 
+        if del = n then count + 1
+        else    let del1 = del + 1
+                if (n % del) = 0 then 
+                                        let count1 = count + 1
+                                        delCount n del1 count1
+                else
+                                        delCount n del1 count
+    delCount n 1 0
+
+let f6 list1 list2 list3 = List.zip3 (List.rev (List.sort list1)) (List.sortBy (fun x -> (cifrSum x)) list2) (List.rev (List.sortBy (fun x -> (delCount x)) list3))
+
+type 'string btree = 
+    Node of 'string * 'string btree * 'string btree
+    | Nil
+
+let prefix root left right = (root(); left(); right())
+let infix root left right = (left(); root(); right())
+let postfix root left right = (left(); right(); root())
+
+
+
+[<EntryPoint>]
+
+let main argv = 
+    let l = readData
+    System.Console.WriteLine(f2 l)
+    writeList (f3 l)
